815bc45dab56629292fff7c71a97b8c0aaa8ab8a
diego64 <diegoferreira1964@gmail.com>
2020-07-13 13:16:11 -0300

    Acertando o nome da classe

--- src/utils/errors/missing-param-error.js
+++ src/utils/errors/missing-param-error.js
@@ -1,4 +1,4 @@
-module.exports = class MissingParamsError extends Error {
+module.exports = class MissingParamError extends Error {
   constructor (paramName) {
     super(`Missing param: ${paramName}`)
     this.name = 'MissingParamsError'
